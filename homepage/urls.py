a6bae82f5731e39d8a5597758736447c288dbc96
Artem Zverev <themezv@gmail.com>
2016-11-19 01:01:10 +0700

    Добавление плитки с главой страницы

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
-from homepage.views import index, about_edit, contacts_edit
+from homepage.views import index, about_edit, contacts_edit, new_tile
 from django.contrib.flatpages.views import flatpage
 
 
 urlpatterns = [
     url(r'^$', index, name='index'),
+    url(r'^new_tile/$', new_tile, name='new_tile'),
     url(r'^about/$', flatpage, {'url': '/about/'}, name='about'),
     url(r'^about/edit$', about_edit, name='about_edit'),
     url(r'^contacts/$', flatpage, {'url': '/contacts/'}, name='contacts'),
--- homepage/views.py
+++ homepage/views.py
@@ -1,15 +1,41 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.csrf import csrf_protect
+from django.http import HttpResponse, Http404
 from .models import Tiles
 from django.contrib.flatpages.models import FlatPage
 from .forms import AboutForm, ContactForm, TilesForm
+from shop.models import Service
+from django.core.exceptions import ObjectDoesNotExist
 
 
 @csrf_protect
 def index(request):
     tiles = Tiles.objects.all()
     form = TilesForm
-    return render(request, 'homepage/index.html', {'tiles': tiles, 'form': form,})
+
+    return render(request, 'homepage/index.html', {'tiles': tiles, 'form': form, })
+
+
+def new_tile(request):
+    if request.method == 'POST':
+        """page_number = request.POST.get('pages')
+        service = Service.objects.get(pk=page_number)
+        try:
+            Tiles.objects.get(pages=service)
+        except ObjectDoesNotExist:
+            Tiles.objects.create(pages=service)
+            return HttpResponse("New tile was saved")
+        else:
+            return HttpResponse("Zanyato")
+        """
+        form = TilesForm(request.POST)
+        if form.is_valid():
+            tile = form.save(commit=True)
+            return redirect('/')
+        else:
+            return HttpResponse("Плитка со ссылкой на эту услугу уже создана <a href=\"/\">Вернуться на главую </a>")
+    else:
+        return Http404
 
 
 def about_edit(request):
